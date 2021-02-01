dbb203f1a236d6c7a25cfbaf562bbe9f0436c585
buddy <starsskulls@gmail.com>
2021-02-01 18:06:18 +0100

    Update compare_db.py

--- con_mods/compare_db.py
+++ con_mods/compare_db.py
@@ -26,26 +26,3 @@ def update_post_db(PageSoup):
 			pass
 
 	return Missing
-
-#
-# if db.PostID == PostID:
-# 	# print(PostID, "already in DB")
-# 	pass
-#
-# 	if db.PostComments == PostComments:
-# 		# print(PostComments, "comments already in DB")
-# 		pass
-#
-# 	else:
-# 		MissingPost['PostID'] = PostID
-# 		MissingPost['Link'] = post['PostLink']
-# 		# print(post['PostLink'])
-# 		# print("PostComments in DB  :", db.PostComments)
-# 		# print("PostComments in Post:", PostComments)
-# else:
-# 	MissingPost['PostID'] = PostID
-# 	MissingPost['Link'] = post['PostLink']
-# 	# print(PostID, "not in DB")
-# 	# db_util.insert_post_tbl(post)
-#
-# Missing.append(MissingPost)
